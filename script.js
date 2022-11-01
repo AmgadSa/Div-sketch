422f2c35b9a7cdbc2170a8979963ff38586b82fa
AmgadSa <AmgadSa@outlook.com>
2022-11-01 21:02:32 +0300

    Refine style and add user interactive options

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-const body = document.querySelector('body');
+const container = document.querySelector('.container');
+const rangeInput = document.querySelector('#rangeid');
+const label = document.querySelector('#rangeLabel');
+rangeInput.value = 16;
 
 function createGrid(){
-    body.textContent = ''
+    divCount = rangeInput.value;
+    container.textContent = '';
+    let divSize = 100/divCount + '%';
     let divX;
-    for(let i = 0; i<(16*16); i++){
-        divX = document.createElement('div');
-        divX.setAttribute('id', i+1);
-        divX.className = 'grid-div';
-        divX.addEventListener('mouseenter',(event)=>{
-            event.target.style['background-color'] = 'black';
-        });
-        body.appendChild(divX);
-    }
+for(let i = 0; i<(divCount*divCount); i++){
+    divX = document.createElement('div');
+    divX.setAttribute('id', i+1);
+    divX.addEventListener('mouseenter',colorTarget);
+    divX.style['width'] = divSize;
+    divX.style['height'] = divSize;
+    container.appendChild(divX);
+    }}
+
+
+function colorTarget(event){
+    let color = event.target.style['background-color'] || 'ff';
+    let rgb = parseInt(color.slice(4,8)).toString(16); //browser switching color to rgb throughs NaN
+    let step = parseInt(color[1],16) || parseInt(rgb[0],16);
+    console.log(color,step)
+    step = (step<=3)? '000000':
+            (step-3)*1118481;
+    color = '#'.concat(step.toString(16));
+    event.target.style['background-color'] = color;
+    console.log(event.target.style['background-color'])
 }
+
 createGrid()
