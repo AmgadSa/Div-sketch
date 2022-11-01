d6f2ac296425dedb4adf22a0501034013f477c05
AmgadSa <AmgadSa@outlook.com>
2022-11-01 16:58:36 +0300

    Add evenListner logic for drawing

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const body = document.querySelector('body');
 
 function createGrid(){
+    body.textContent = ''
     let divX;
-    for(let i = 0; i<16; i++){
-        divX = document.createElement('div')
-        divX.setAttribute('id', i);
+    for(let i = 0; i<(16*16); i++){
+        divX = document.createElement('div');
+        divX.setAttribute('id', i+1);
         divX.className = 'grid-div';
+        divX.addEventListener('mouseenter',(event)=>{
+            event.target.style['background-color'] = 'black';
+        });
         body.appendChild(divX);
     }
 }
+createGrid()
